c5cbb38b9d833e6ade042296b5fc12821375f11e
slabwu <134193743+slabwu@users.noreply.github.com>
2026-01-16 11:14:09 +0800

    Initialise RTF

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { Canvas } from '@react-three/fiber'
+import './style.css'
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'))
+
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Canvas>
+      <mesh>
+          <torusKnotGeometry />
+          <meshNormalMaterial />
+      </mesh>
+    </Canvas>
+  </StrictMode>
 )
